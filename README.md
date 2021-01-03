b305fbda6361d54a1973aec5023e645120a53ade
Peyton Wang <peyton.wang@wellesley.edu>
2021-01-02 17:42:30 -0800

    Updated documentation and command-line arguments

--- README.md
+++ README.md
@@ -4,34 +4,25 @@ This package contains scraper scripts that uses Selenium to scrape user profile 
 ## Set-up
 ---
 ### Interactive:
-1) Run `selenium_scraper.sh`.
+1) Download ChromeDriver for your version of Chrome and ensure that it is executable from the `resume-bias` directory. 
+
+2) Run `selenium_scraper.sh`.
     ```
     . selenium_scraper.sh
     ```
 
-2) Enter username and password when prompted to in the console.
+3) Enter username, password, filename, and search_query when prompted to in the console.
 
-### Specify virtual environment and arguments in the command line:
-1) Install dependencies.
+### Command Line:
+1) Download ChromeDriver for your version of Chrome and ensure that it is executable from the `resume-bias` directory. 
+
+2) Install dependencies.
     ```
     pip3 install -r requirements.txt
     ```
 
-2) There are three options (a, b, c) to run `selenium_scraper.py` from the `resume-bias` directory.
-
-    a) Export credentials, then run `selenium_scraper.py`.
+3) Run `selenium_scraper.py` with command-line arguments from the `resume-bias` directory. 
     ```
-    export USERNAME=<username> 
-    export PASSWORD=<password>
-    python3 selenium_scraper.py
+    python3 selenium_scraper.py -u <username> -p <password> -f <filename> -s <search_query>
     ```
-
-    b) Run `selenium_scraper.py` with command-line arguments.
-    ```
-    python3 selenium_scraper.py -u <username> -p <password>
-    ```
-
-    c) Run `selenium_scraper.py`, then enter username and password when prompted to in the console.
-    ```
-    python3 selenium_scraper.py
-    ```
+    The `filename` argument is the name of a txt file containing LinkedIn URLs on separate lines (e.g. `users.txt`). The `search_query` argument is the search query for Googling LinkedIn users (e.g. site:linkedin.com/in/ AND "python developer"). If both arguments are specified, the program combines the URLs specified in the txt file and from the Google search query.
--- selenium_scraper.py
+++ selenium_scraper.py
@@ -9,19 +9,27 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 class SeleniumScraper():
+    
     def __init__(self, args=None, **kwargs):
         if isinstance(args, argparse.Namespace):
             self.username = args.username
             self.password = args.password
+            self.filename = args.filename
+            self.search_query = args.search_query
         else:
             self.username = kwargs.get('username')
             self.password = kwargs.get('password')
+            self.filename = kwargs.get('filename')
+            self.search_query = kwargs.get('search_query')
 
         self.driver = webdriver.Chrome('{}/chromedriver'.format(os.getcwd()))
         self.login()
-        self.generate_data('users.txt')
+        self.generate_data()
                 
     def login(self):
+        """
+        Automates logging in to a LinkedIn account in a Chrome guest browser.
+        """
         logging.info("Logging into your LinkedIn account...")
 
         self.driver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
@@ -37,6 +45,10 @@ class SeleniumScraper():
         password.submit()
 
     def read_user_list(self, filename):
+        """
+        Given the name of a txt file with a LinkedIn URL on each line, reads in the users.
+        Returns a list of the URLs in the txt files.
+        """
         logging.info("Reading in users from {}...".format(filename))
         users = []
         
@@ -44,11 +56,15 @@ class SeleniumScraper():
             with open(filename, 'r') as in_file:
                 users = [line.strip() for line in in_file]
         else:
-            logging.error("users.txt file doesn't exist.")
+            logging.error("{} file doesn't exist.".format(filename))
         
         return users
 
     def google_search_users(self, search_query):
+        """
+        Given a search query, automates searching on Google for a user associated with specific keywords.
+        Returns a list of LinkedIn URLs.
+        """
         logging.info("Google searching LinkedIn profiles with query {}...".format(search_query))
         users = []
 
@@ -69,17 +85,28 @@ class SeleniumScraper():
         return users
 
     def scrape(self, linkedin_url):
+        """
+        Given a LinkedIn URL for a user, scrapes data on that user.
+        Returns a pandas DataFrame for each user.
+        """
         logging.info("Scraping data from {}...".format(linkedin_url))
         self.driver.get(linkedin_url)
         df = pd.DataFrame()
         
-    def generate_data(self, users_txt_file=None):
+    def generate_data(self):
+        """
+        Driver function for the program.
+        """
         users = []
 
-        if users_txt_file:
-            users = self.read_user_list(users_txt_file)
+        if self.filename and self.search_query:
+            users = self.read_user_list(self.filename) + self.google_search_users(self.search_query)
+        elif self.filename:
+            users = self.read_user_list(self.filename)
+        elif self.search_query:
+            users = self.google_search_users(self.search_query)
         else:
-            users = self.google_search_users('site:linkedin.com/in/ AND "python developer" AND "London"')
+            logging.warning("No users to scrape...")
             
         for user in users:
             self.scrape(user) 
@@ -89,19 +116,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--username', help="Enter LinkedIn username")
     parser.add_argument('-p', '--password', help="Enter LinkedIn password")
+    parser.add_argument('-f', '--filename', help="Enter name of txt file containing LinkedIn URLs")
+    parser.add_argument('-s', '--search_query', help="Enter search query for Googling LinkedIn users")
     args = parser.parse_args()
-    kwargs = {}
 
-    USERNAME = os.getenv('USERNAME')  # $export USERNAME=<username>
-    PASSWORD = os.getenv('PASSWORD')  # $export PASSWORD=<password>
-    
-    if USERNAME and PASSWORD:
-        kwargs['username'] = USERNAME
-        kwargs['password'] = PASSWORD
-        obj = SeleniumScraper(**kwargs)
-    elif args.username and args.password:
+    if args.username and args.password:
         obj = SeleniumScraper(args)
     else: 
+        kwargs = {}
         kwargs['username'] = input("Enter your username: ")
         kwargs['password'] = input("Enter your password: ")
+        answer = input("Where will you be getting your LinkedIn URLs to scrape? Type 'file' for txt file or 'search' for Google search query: ")
+        if answer.strip().lower() == 'file':
+            kwargs['filename'] = input("Enter the name of your txt file containing LinkedIn URLs: ")
+        else: 
+            kwargs['search_query'] = input("Enter your search query for Googling LinkedIn users: ")
         obj = SeleniumScraper(**kwargs)
