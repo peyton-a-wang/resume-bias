30a71de865313d10cd3a6c44d3bcc6b06d612608
Peyton Wang <peyton.wang@wellesley.edu>
2021-02-22 00:06:40 -0800

    Updated README

--- linkedin_user.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-class LinkedinUser():
-    
-    def __init__(self, profile_url):
-        self.profile_url = profile_url
-                
-    def scrape():
-        return NotImplementedError
